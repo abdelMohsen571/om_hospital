c0e172fb8dbaa01dbe33cf945c384a58ca4a24c4
mohsen <amohsenmohamed571@gmail.com>
2024-01-28 11:14:47 +0200

    add chatter and tracking fields
    chatter addedd in views in form view under sheet and tracking its an widget in fields like requried

--- models/patiant.py
+++ models/patiant.py
@@ -3,9 +3,10 @@ from odoo import models, fields, api
 
 class HospitalPatiant(models.Model):
     _name = 'hospital.patiant'
+    _inherit = ['mail.thread','mail.activity.mixin']
     _description = 'Pationt description'
 
-    name = fields.Char(string="name")
+    name = fields.Char(string="name",tracking=True)
     age = fields.Integer(string="age")
     description = fields.Text()
     gender=fields.Selection([('male','Male'),('female','Female')])
