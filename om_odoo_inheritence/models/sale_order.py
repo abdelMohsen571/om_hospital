c02875694e4f1165062cdb34c9c7197248df859e
mohsen <amohsenmohamed571@gmail.com>
2024-02-29 16:35:31 +0200

    inherit method action_confirm

--- om_odoo_inheritence/models/sale_order.py
+++ om_odoo_inheritence/models/sale_order.py
@@ -6,3 +6,8 @@ class SaleOrder(models.Model):
     _description = 'inherit sale order'
 
     confirmed_user_id = fields.Many2one('res.users',string='Confirmed User')
+
+    def action_confirm(self):
+        print('inherited funcation success')
+        super(SaleOrder,self).action_confirm()
+        self.confirmed_user_id=self.env.user.id
