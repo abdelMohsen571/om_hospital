bfe1ce3eff9c9bcb981c17bae90a3f4f26c3089c
mohsen <amohsenmohamed571@gmail.com>
2024-03-05 15:19:46 +0200

    Apply Domain For appointment_id in cancel_wizard.py
    to show draft appointment or with low priority

--- wizard/cancel_appointment_wizard.py
+++ wizard/cancel_appointment_wizard.py
@@ -1,26 +1,30 @@
-from odoo import api, fields, models,_
-from datetime import date,datetime,timedelta
+from odoo import api, fields, models, _
+from datetime import date, datetime, timedelta
 from odoo.exceptions import ValidationError
 import datetime
+
+
 class CancelAppointment(models.TransientModel):
     _name = "cancel.appointment.wizard"
 
-    appointment_id=fields.Many2one('hospital.appointment',string='Appointment')
-    reason=fields.Text(string='reason')
-    date_cancel=fields.Date(string='Cancellation Date')
+    appointment_id = fields.Many2one('hospital.appointment', string='Appointment',
+                                     domain=['|',('state', '=', 'draft'), ('priority', 'in', ('0', '1','')), ])
+    reason = fields.Text(string='reason')
+    date_cancel = fields.Date(string='Cancellation Date')
 
     def cancel_appointment(self):
         for rec in self:
-            if rec.appointment_id.booking_date==fields.Date.today():
+            if rec.appointment_id.booking_date == fields.Date.today():
                 raise ValidationError(_('sorry, cancellation is not allowed on the same day of booking'))
             return
+
     @api.model
     def default_get(self, fields_list):
-        res=super(CancelAppointment,self).default_get(fields_list)
-        res['date_cancel']=datetime.date.today()
-        res['reason']='no reson'
+        res = super(CancelAppointment, self).default_get(fields_list)
+        res['date_cancel'] = datetime.date.today()
+        res['reason'] = 'no reson'
 
-        print('...........',res)
+        print('...........', res)
         if self.env.context.get('active_id'):
-            res['appointment_id']=self.env.context.get('active_id')
+            res['appointment_id'] = self.env.context.get('active_id')
         return res
