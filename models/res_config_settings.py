25d90c72cd01c2bfb101fdbc02d1d01e04000eb7
mohsen <amohsenmohamed671@gmail.com>
2024-03-19 13:11:13 +0200

    add attribute config_parameter
     to make field stored in database

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -7,4 +7,4 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = ['res.config.settings']
 
-    cancel_days=fields.Integer(string='Cancel Days')
+    cancel_days=fields.Integer(string='Cancel Days',config_parameter='om_hospital.cancel_days')
